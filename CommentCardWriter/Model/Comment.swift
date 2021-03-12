e5efc494b7be878b32ffc7b758d054ed3a784db1
Jasper Hersov <Hersov.J@etoncollege.org.uk>
2021-03-12 12:29:42 +0000

    Project Started

---
+++ CommentCardWriter/Model/Comment.swift
@@ -0,0 +1,16 @@
+//
+//  Comment.swift
+//  CommentCardWriter
+//
+//  Created by Hersov, Jasper (HWTA) on 12/03/2021.
+//
+
+import Foundation
+
+class Comment {
+    let subject: String
+    let effort: Int
+    let enjoyment: Int
+    let anticipation: String
+
+}
